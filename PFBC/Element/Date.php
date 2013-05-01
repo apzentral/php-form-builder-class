12e9b07092061e227edc2b895a735a81180ca425
Prut Udomwattawee <apzentral@gmail.com>
2013-05-01 10:28:07 -0700

    Adding dynamic datepicker call.

--- PFBC/Element/Date.php
+++ PFBC/Element/Date.php
@@ -6,13 +6,23 @@ class Date extends Textbox {
 		"type" => "date",
 		"pattern" => "(0[1-9]|1[012])[/](0[1-9]|[12][0-9]|3[01])[/](19|20)\d\d$"
 	);
+	protected $parentBind; // Attached event to parent
+	protected $childBind; // Attached event to child
+	protected $eventBind; // Attached event
 	protected $jQueryOptions;
 
 	public function __construct($label, $name, array $properties = null) {
 		$this->_attributes["placeholder"] = "mm/dd/yyyy";
 		$this->_attributes["title"] = $this->_attributes["placeholder"];
 
+		$this->eventBind = "click";
 		parent::__construct($label, $name, $properties);
+		$options = array(
+			'changeMonth' => 'true',
+			'changeYear' => 'true',
+			'dateFormat' => "mm/dd/yy"
+		);
+		$this->jQueryOptions = array_merge((array)$this->jQueryOptions, $options);
     }
 
 	public function render() {
@@ -21,16 +31,16 @@ class Date extends Textbox {
 	}
 
 	function jQueryDocumentReady() {
-		if(is_null($this->jQueryOptions))
+		// Use attribute parent-bind for dynamic dom element
+		if(is_null($this->parentBind))
 		{
-			$options = array(
-				'changeMonth' => 'true',
-				'changeYear' => 'true',
-				'dateFormat' => "mm/dd/yy"
-			);
-			$this->jQueryOptions = $options;
+			echo 'jQuery("#', $this->_attributes["id"], '").datepicker(', $this->jQueryOptions(), ');';
 		}
-		echo 'jQuery("#', $this->_attributes["id"], '").datepicker(', $this->jQueryOptions(), ');';
+		else
+		{
+			$child_target = (is_null($this->childBind)) ? '#'.$this->_attributes['id']: $this->childBind;
+			echo 'jQuery("',$this->parentBind,'").on("'.$this->eventBind.'", "',$child_target,'", function() { jQuery("#', $this->_attributes["id"], '").removeClass("hasDatepicker").datepicker(', $this->jQueryOptions(), ');});';
+		}
 	}
 
 	function getJSFiles() {
