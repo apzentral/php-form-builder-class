6f776ce9bf7e1a13fb17ccd4bc19a03e97e896b4
Prut Udomwattawee <apzentral@gmail.com>
2013-05-01 15:02:53 -0700

    Adding jQuery Effect for KO binding

--- PFBC/Resources/knockout.custom.js
+++ PFBC/Resources/knockout.custom.js
@@ -42,3 +42,42 @@ ko.bindingHandlers.uniqueDiv = {
         }
     }
 };
+
+/*===== Require jQuery =====*/
+ko.bindingHandlers.slideVisible = {
+    update: function(element, valueAccessor, allBindingsAccessor) {
+        // First get the latest data that we're bound to
+        var value = valueAccessor(), allBindings = allBindingsAccessor();
+
+        // Next, whether or not the supplied model property is observable, get its current value
+        var valueUnwrapped = ko.utils.unwrapObservable(value);
+
+        // Grab some more data from another binding property
+        var duration = allBindings.slideDuration || 500; // 500ms is default duration unless otherwise specified
+
+        // Now manipulate the DOM element
+        if (valueUnwrapped == true)
+            $(element).slideDown(duration); // Make the element visible
+        else
+            $(element).slideUp(duration);   // Make the element invisible
+    }
+};
+
+ko.bindingHandlers.fadeVisible = {
+    update: function(element, valueAccessor, allBindingsAccessor) {
+        // First get the latest data that we're bound to
+        var value = valueAccessor(), allBindings = allBindingsAccessor();
+
+        // Next, whether or not the supplied model property is observable, get its current value
+        var valueUnwrapped = ko.utils.unwrapObservable(value);
+
+        // Grab some more data from another binding property
+        var duration = allBindings.slideDuration || 500; // 500ms is default duration unless otherwise specified
+
+        // Now manipulate the DOM element
+        if (valueUnwrapped == true)
+            $(element).fadeIn(duration); // Make the element visible
+        else
+            $(element).fadeOut(duration);   // Make the element invisible
+    }
+};
